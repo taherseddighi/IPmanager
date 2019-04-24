c1197be318ddb0aac64b56a5aea83ba3c267681e
Taher <taherseddighi@gmail.com>
2019-04-24 16:57:11 +0430

    gitignore

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Available_Public_IP, Used_Public_IP, Used_Private_IP
 # Register your models here.
+admin.site.register(Available_Public_IP)
+admin.site.register(Used_Public_IP)
+admin.site.register(Used_Private_IP)
--- web/models.py
+++ web/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Available_Public_IP(models.Model):
+    IP = models.CharField(max_length= 255, null = True)  
+    date_start = models.DateTimeField(null = True, blank = True)
+    date_expire = models.DateTimeField(null = True, blank = True)
+    price_bought = models.BigIntegerField(null = True, blank = True)
+    price_to_sell = models.BigIntegerField(null = True, blank = True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null = True, blank = True)
+    description = models.CharField(max_length= 255, null = True, blank = True)
+    def __str__(self):
+            return "IP :{}-will expire on {}".format(self.IP, self.date_expire)
+
+class Used_Public_IP(models.Model):
+    IP = models.CharField(max_length= 255, null = True)
+    client = models.CharField(max_length = 255, null = True, blank = True)
+    date_start = models.DateTimeField(null = True, blank = True)
+    date_expire = models.DateTimeField(null = True, blank = True)
+    price_bought = models.BigIntegerField(null = True, blank = True)
+    price_sold = models.BigIntegerField(null = True, blank = True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null = True, blank = True)
+    description = models.CharField(max_length= 255, null = True, blank = True)
+    def __str__(self):
+        return "IP :{}-will expire on {}".format(self.IP, self.date_expire)
+
+class Used_Private_IP(models.Model):
+    IP = models.CharField(max_length= 255, null = True) 
+    Device = models.CharField(max_length = 255, null = True, blank = True)
+    date_start = models.DateTimeField(null = True, blank = True)
+    date_expire = models.DateTimeField(null = True, blank = True)
+    price_bought = models.BigIntegerField(null = True, blank = True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null = True, blank = True)
+    description = models.CharField(max_length= 255, null = True, blank = True)
+    def __str__(self):
+        return "IP :{}-will expire on {}".format(self.IP, self.date_expire)
