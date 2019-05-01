3c4643e55a52d87ec2242c19fa9ecc31265fa8ca
Taher <taherseddighi@gmail.com>
2019-05-01 14:28:28 +0430

    1

--- web/views.py
+++ web/views.py
@@ -3,7 +3,7 @@ from json import JSONEncoder
 from datetime import datetime
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from web.models import User, Available_Public_IP
+from web.models import User, Token, Available_Public_IP
 
 
 # Create your views here.
@@ -11,10 +11,10 @@ from web.models import User, Available_Public_IP
 @csrf_exempt
 def submit_Available_Public_IP(request):
     #validate data later
-    #this_token = request.POST['token']
-    #this_user = User.objects.filter(token__token=this_token).get()
-    #now = datetime.now()
-    #Available_Public_IP.objects.Create(User=this_user, IP=request.POST['IP'], date_start=now)
+    this_token = request.POST['token']
+    this_user = User.objects.filter(token__token=this_token).get()
+    now = datetime.now()
+    Available_Public_IP.objects.Create(User=this_user, IP=request.POST['IP'], date_start=now)
     print('HI Can you See Me ? I am Here. Holllaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
     print(request.POST)
 
