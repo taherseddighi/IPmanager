bc1b01da32d2badd2d539b2b59bbb6c068c68fae
Taher <taherseddighi@gmail.com>
2019-05-12 16:07:37 +0430

    views

--- web/views.py
+++ web/views.py
@@ -13,8 +13,11 @@ def submit_Available_Public_IP(request):
     #validate data later
     this_token = request.POST['token']
     this_user = User.objects.filter(token__token=this_token).get()
-    now = datetime.now()
-    Available_Public_IP.objects.Create(User=this_user, IP=request.POST['IP'], date_start=now)
+    if request.POST['date_start']:
+        Available_Public_IP.objects.create(user=this_user, IP=request.POST['IP'], date_start=request.POST['date_start'], date_expire=request.POST['date_expire'], price_bought=request.POST['price_bought'], price_to_sell=request.POST['price_to_sell'], description=request.POST['description'])
+    else:
+        now = datetime.now()
+        Available_Public_IP.objects.create(user=this_user, IP=request.POST['IP'], date_start=now, date_expire=request.POST['date_expire'], price_bought=request.POST['price_bought'], price_to_sell=request.POST['price_to_sell'], description=request.POST['description'])
     print('HI Can you See Me ? I am Here. Holllaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
     print(request.POST)
 
